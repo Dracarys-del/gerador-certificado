5c7af0c7f8cb698a5bf602231561ada74f271fff
dracarys <hectorlofredo@gmail.com>
2026-04-09 10:25:45 -0300

    minimal interaction with the page

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,10 +8,11 @@ import { ItemCertificado } from './_components/item-certificado/item-certificado
 import { BaseUi } from './_components/base-ui/base-ui';
 import { Certificados } from './pages/certificados/certificados';
 import { CertificadoForm } from "./pages/certificado-form/certificado-form";
+import { Certificado } from "./pages/certificado/certificado";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, PrimaryButton, RouterOutlet, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
+  imports: [Navbar, CommonModule, RouterOutlet, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, NgStyle, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
-export class CertificadoForm {}
+export class CertificadoForm {
+  nome: string ='';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Cobol'];
+}
